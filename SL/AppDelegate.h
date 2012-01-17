9d16ceb80d0d6585b9f13cf3cd56a7a616fc9d8a
Axel Moller <axel@spotremote.com>
2012-01-17 16:32:56 +0100

    Initial Commit

---
+++ SL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SL
+//
+//  Created by Axel Möller on 1/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
---
+++ SL/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  SL
+//
+//  Created by Axel Möller on 1/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
